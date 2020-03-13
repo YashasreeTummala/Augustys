c130bc778b79d5c5e192ffcf816661f420e2c665
AdityaKumar3112 <61984990+AdityaKumar3112@users.noreply.github.com>
2020-03-13 19:00:12 +0530

    I removed SOP statement

--- MyFirstGit.java
+++ MyFirstGit.java
@@ -5,6 +5,5 @@ public class MyFirstGitB2
 	public static void main(String[] args)
 	{
 		System.out.println("1. Let's complete what I Started..");
-		System.out.println("2. Let's complete what I Started..");
 	}
 }
